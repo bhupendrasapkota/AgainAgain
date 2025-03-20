8d624812609a33a635989280b6aa24dbcc8f002b
Bhupendra sapkota <np05cp4a230235@iic.edu.np>
2025-03-20 07:15:19 +0545

    updated

--- backend/apps/features/photos/signals.py
+++ backend/apps/features/photos/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Photo, Collection
+from .models import Photo
 import logging
 
 logger = logging.getLogger(__name__)
